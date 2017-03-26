9d718530b4bb8beb5601a0b0b300a9a0e93dd4d9
jwile14 <wilejd@wilejd-w540.rose-hulman.edu>
2017-03-26 19:28:46 -0400

    testGameControllerCreation fails

---
+++ src/tests/GameControllerTest.java
@@ -0,0 +1,10 @@
+package tests;
+
+import org.junit.Test;
+
+public class GameControllerTest {
+	@Test
+	public void testGameControllerCreation() {
+		GameController gameController = new GameController();
+	}
+}

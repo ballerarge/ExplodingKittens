dc4ff77a92dc8d676f8ffeb783b789be1b54d018
decramrj <decramrj@rose-hulman.edu>
2017-03-28 10:34:47 -0400

    testDeckConstructor fails

---
+++ src/tests/DeckTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import code.Deck;
+
+public class DeckTest {
+
+	@Test
+	public void testDeckConstructor() {
+		Deck deck = new Deck();
+	}
+
+}

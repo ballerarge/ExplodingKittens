f53af3d9b6c01650ab4da6758f3a229365725989
KhaledAlfayez237 <alfayeka@ALFAYEKA-W541.rose-hulman.edu>
2017-03-27 21:54:16 -0400

    testHandManagerCreation and testPlayerCreation fails.

---
+++ src/tests/HandManagerTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import code.HandManager;
+
+public class HandManagerTest {
+
+	@Test
+	public void testHandManagerCreation() {
+		HandManager handMng = new HandManager();
+	}
+
+}
---
+++ src/tests/PlayerTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import code.Player;
+
+public class PlayerTest {
+
+	@Test
+	public void testPlayerCreation() {
+		Player player = new Player();
+	}
+
+}

b4cd2d0ec51fa6e24667e9eca14c13c8b84dbc56
decramrj <decramrj@rose-hulman.edu>
2017-03-28 10:38:54 -0400

    testGetCards fails

--- src/tests/DeckTest.java
+++ src/tests/DeckTest.java
@@ -2,15 +2,26 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import code.Deck;
+import code.Card;
 
 public class DeckTest {
 
 	@Test
 	public void testDeckConstructor() {
 		Deck deck = new Deck();
+	}
+	
+	@Test
+	public void testGetCards() {
+		List<Card> cards;
+		Deck deck = new Deck(cards);
+		
+		assertEquals(cards, deck.getCards());
 	}
 
 }

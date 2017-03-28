dcbe0a2050ac2b6dd3ae0fac44ff0aa5b1c2e05b
decramrj <decramrj@rose-hulman.edu>
2017-03-28 12:10:35 -0400

    testGetCards fails

--- src/tests/MainDeckTest.java
+++ src/tests/MainDeckTest.java
@@ -1,9 +1,11 @@
 package tests;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 
+import code.Card;
 import code.MainDeck;
 
 public class MainDeckTest {
@@ -11,6 +13,15 @@ public class MainDeckTest {
 	@Test
 	public void testMainDeckConstructor() {
 		MainDeck mDeck = new MainDeck();
+	}
+	
+	@Test
+	public void testGetCards() {
+		List<Card> cards = new ArrayList<Card>();
+		MainDeck mDeck = new MainDeck(cards);
+		
+		assertEquals(cards, mDeck.getCards());
+		
 	}
 
 }

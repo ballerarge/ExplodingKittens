a72e72db389de3c78e42783d1681fc9e015dea76
jwile14 <wilejd@wilejd-w540.rose-hulman.edu>
2017-03-27 20:55:47 -0400

    Added testCardFactoryCreation - fails

---
+++ src/tests/CardFactoryTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.junit.Test;
+
+
+public class CardFactoryTest {
+	@Test
+	public void testCardFactoryCreation() {
+		CardFactory cardFactory = new CardFactory();
+	}
+}

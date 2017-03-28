8d3b87cb6427b26d39d595ff7cf7e186ad239c54
decramrj <decramrj@rose-hulman.edu>
2017-03-28 12:12:51 -0400

    MainDeck - testGetCards() passes

--- src/code/MainDeck.java
+++ src/code/MainDeck.java
@@ -1,5 +1,20 @@
 package code;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainDeck {
+	
+	public MainDeck() {
+		
+	}
+
+	public MainDeck(List<Card> cards) {
+		
+	}
+
+	public List<Card> getCards() {
+		return new ArrayList<Card>();
+	}
 
 }
--- src/tests/MainDeckTest.java
+++ src/tests/MainDeckTest.java
@@ -1,5 +1,7 @@
 package tests;
 
+import static org.junit.Assert.*;
+
 import java.util.ArrayList;
 import java.util.List;
 

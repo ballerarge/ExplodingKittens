acccc880392b9305e84e0dd9fdb06d1fb7091e3b
decramrj <decramrj@rose-hulman.edu>
2017-03-29 19:31:49 -0400

    Refactored MainDeck, all tests pass

--- src/code/MainDeck.java
+++ src/code/MainDeck.java
@@ -34,23 +34,57 @@ public class MainDeck {
 	}
 
 	public void initStartingDeck() {
+		initFavorCards();
+		initShuffleCards();
+		initSkipCards();
+		initAttackCards();
+		initNormalCards();
+		initSeeTheFutureCards();
+		initNopeCards();
+	}
+	
+	private void initFavorCards() {
 		for (int i = 0; i < 4; i++) {
 			deck.addCard(new FavorCard(), 0);
+		}
+	}
+	
+	private void initShuffleCards() {
+		for (int i = 0; i < 4; i++) {
 			deck.addCard(new ShuffleCard(), 0);
+		}
+	}
+	
+	private void initSkipCards() {
+		for (int i = 0; i < 4; i++) {
 			deck.addCard(new SkipCard(), 0);
+		}
+	}
+	
+	private void initAttackCards() {
+		for (int i = 0; i < 4; i++) {
 			deck.addCard(new AttackCard(), 0);
 		}
-		
-		// Fix this part in particular, doesn't account
-		// for types of normal cards.
+	}
+	
+	private void initNormalCards() {
 		for (int i = 0; i < 20; i++) {
 			deck.addCard(new NormalCard(), 0);
 		}
-		
+	}
+	
+	private void initSeeTheFutureCards() {
 		for (int i = 0; i < 5; i++) {
 			deck.addCard(new SeeTheFutureCard(), 0);
+		}
+	}
+	
+	private void initNopeCards() {
+		for (int i = 0; i < 5; i++) {
 			deck.addCard(new NopeCard(), 0);
 		}
 	}
+	
+	
 
 }

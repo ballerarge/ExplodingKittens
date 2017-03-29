d939afdfdeaeec30991ba551f351b1ff3d176e12
jwile14 <wilejd@wilejd-w540.rose-hulman.edu>
2017-03-29 15:40:15 -0400

    added testCreateCardMultipleNope - fails

--- src/tests/CardFactoryTest.java
+++ src/tests/CardFactoryTest.java
@@ -48,12 +48,24 @@ public class CardFactoryTest {
 		exception.expect(IncorrectNumberOfCards.class);
 		List<Card> cards = cardFactory.createCard(CardFactory.NORMAL_CARD, -1);
 	}
-	
+
 	@Test
 	public void testCreateCardSingleNope() {
 		CardFactory cardFactory = new CardFactory();
 
 		Card card = cardFactory.createCard(CardFactory.NOPE_CARD);
 		assertTrue(NopeCard.class.isInstance(card));
+	}
+
+	@Test
+	public void testCreateCardMultipleNope() throws IncorrectNumberOfCards {
+		CardFactory cardFactory = new CardFactory();
+
+		List<Card> cards = cardFactory.createCard(CardFactory.NOPE_CARD, 10);
+
+		for (Card card : cards) {
+			assertTrue(NopeCard.class.isInstance(card));
+		}
+		assertEquals(10, cards.size());
 	}
 }

12192aebdd613ac8cb580ab73cd2486449d5c667
jwile14 <wilejd@wilejd-w540.rose-hulman.edu>
2017-03-29 15:12:58 -0400

    added testCreateCardMultipleNormal - fails

--- src/tests/CardFactoryTest.java
+++ src/tests/CardFactoryTest.java
@@ -1,11 +1,13 @@
+
 package tests;
 
 import static org.junit.Assert.*;
 
+import java.awt.List;
+
 import org.junit.Test;
 
 import code.*;
-
 
 public class CardFactoryTest {
 	@Test
@@ -13,12 +15,24 @@ public class CardFactoryTest {
 		@SuppressWarnings("unused")
 		CardFactory cardFactory = new CardFactory();
 	}
-	
+
 	@Test
 	public void testCreateCardSingleNormal() {
 		CardFactory cardFactory = new CardFactory();
-		
+
 		Card card = cardFactory.createCard(CardFactory.NORMAL_CARD);
 		assertTrue(NormalCard.class.isInstance(card));
+	}
+
+	@Test
+	public void testCreateCardMultipleNormal() {
+		CardFactory cardFactory = new CardFactory();
+		
+		List<Card> cards = cardFactory.createCard(CardFactory.NORMAL_CARD, 10);
+		
+		for (Card card: cards) {
+			assertTrue(NormalCard.class.isInstance(card));
+		}
+		assertEquals(10, cards.size());
 	}
 }

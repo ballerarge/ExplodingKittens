aa8bc4ab769f3c79d3a2f107d17a140dec47ba7a
jwile14 <wilejd@wilejd-w540.rose-hulman.edu>
2017-03-29 15:06:11 -0400

    Added testCreateCardSingleNormal - fails

--- src/tests/CardFactoryTest.java
+++ src/tests/CardFactoryTest.java
@@ -1,11 +1,23 @@
 package tests;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
+
+import code.*;
 
 
 public class CardFactoryTest {
 	@Test
 	public void testCardFactoryCreation() {
 		CardFactory cardFactory = new CardFactory();
+	}
+	
+	@Test
+	public void testCreateCardSingleNormal() {
+		CardFactory cardFactory = new CardFactory();
+		
+		Card card = cardFactory.createCard(cardFactory.NORMAL_CARD);
+		assertTrue(NormalCard.class.isInstance(card));
 	}
 }

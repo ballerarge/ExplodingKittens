9fcaf6f66ad86a90fa992562ebba5f69fdff7126
jwile14 <wilejd@wilejd-w540.rose-hulman.edu>
2017-03-29 15:09:44 -0400

    testCreateCardSingleNormal passes

--- src/code/CardFactory.java
+++ src/code/CardFactory.java
@@ -1,7 +1,13 @@
 package code;
 
 public class CardFactory {
+	public final static int NORMAL_CARD = 0;
+	
 	public CardFactory() {
 		
+	}
+
+	public Card createCard(int cardID) {
+		return new NormalCard();
 	}
 }
---
+++ src/code/NormalCard.java
@@ -0,0 +1,7 @@
+package code;
+
+public class NormalCard implements Card {
+	public NormalCard() {
+		
+	}
+}

e306104b8da083807589f8d3d2b8976ad98ed726
jwile14 <wilejd@wilejd-w540.rose-hulman.edu>
2017-03-29 15:15:19 -0400

    testCreateCardMultipleNormal passes

--- src/code/CardFactory.java
+++ src/code/CardFactory.java
@@ -1,6 +1,9 @@
 
 package code;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CardFactory {
 	public final static int NORMAL_CARD = 0;
 
@@ -10,5 +13,15 @@ public class CardFactory {
 
 	public Card createCard(int cardID) {
 		return new NormalCard();
+	}
+
+	public List<Card> createCard(int cardID, int numCards) {
+		List<Card> cards = new ArrayList<Card>();
+		
+		for (int i = 0; i < numCards; i++) {
+			cards.add(new NormalCard());
+		}
+		
+		return cards;
 	}
 }

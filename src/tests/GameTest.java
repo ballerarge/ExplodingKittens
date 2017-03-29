768ca3979ee90c9c8b2286f5fc00327db63f0f88
Aaron Michael <apmichael9@gmail.com>
2017-03-29 00:28:23 -0400

    Created GameTest. Added testGameCreation(). fails.

---
+++ src/tests/GameTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameTest {
+
+	@Test
+	public void testGameCreation() {
+		Game game=new Game();
+	}
+
+}

9b6bb818aba201cfca7f7e75a9c311d13460164c
KhaledAlfayez237 <alfayeka@ALFAYEKA-W541.rose-hulman.edu>
2017-03-29 21:17:33 -0400

    testPlayerCreationWithName passes.

--- src/code/Player.java
+++ src/code/Player.java
@@ -2,8 +2,14 @@ package code;
 
 public class Player {
 
+	private String name;
+	
 	public Player() {
-		
+		this.name = "default";
+	}
+
+	public Player(String name) {
+		this.name = name;
 	}
 	
 	

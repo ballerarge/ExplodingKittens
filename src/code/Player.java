845f49ba6b45c76dbcc40fdbab3b1ba94f0c39ae
KhaledAlfayez237 <alfayeka@ALFAYEKA-W541.rose-hulman.edu>
2017-03-29 21:39:49 -0400

    testDrawCard passes!

--- src/code/Player.java
+++ src/code/Player.java
@@ -1,5 +1,7 @@
 package code;
 
+import java.util.List;
+
 public class Player {
 
 	private String name;
@@ -23,6 +25,14 @@ public class Player {
 
 	public HandManager getHandManger() {
 		return handManager;
+	}
+
+	public List<Card> getHand() {
+		return handManager.getHand();
+	}
+
+	public void drawCard() {
+		handManager.draw();
 	}
 	
 	

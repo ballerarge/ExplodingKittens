8d94081a4535d8d0393e4b9e08d3ede8915146e3
jwile14 <wilejd@wilejd-w540.rose-hulman.edu>
2017-03-30 20:35:51 -0400

    added testPlayerManagerCreation - passes

---
+++ src/tests/PlayerManagerTest.java
@@ -0,0 +1,14 @@
+
+package tests;
+
+import org.junit.Test;
+
+import code.*;
+
+public class PlayerManagerTest {
+
+	@Test
+	public void testPlayerManagerCreation() {
+		PlayerManager playerManager = new PlayerManager();
+	}
+}

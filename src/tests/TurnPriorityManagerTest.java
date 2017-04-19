54974afaf93d8add55a2301dbcb825bd9d7fd255
Aaron Michael <apmichael9@gmail.com>
2017-04-19 16:15:06 -0400

    Added testConstructor for TurnPriorityManager(). Fails because there is no TPM class yet.

---
+++ src/tests/TurnPriorityManagerTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TurnPriorityManagerTest {
+
+	@Test
+	public void testConstruction() {
+		TurnPriorityManager manager=new TurnPriorityManager();
+	}
+
+}

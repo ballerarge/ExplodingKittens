23b4aaf1afa7cc525d0ef762e0dea44f05d8bd6e
jwile14 <wilejd@wilejd-w540.rose-hulman.edu>
2017-04-20 19:07:29 -0400

    testRemovePlayerFromEmptyPlayerList passes

--- src/code/PriorityManager.java
+++ src/code/PriorityManager.java
@@ -1,13 +1,35 @@
 
 package code;
 
+import java.util.ArrayList;
+
+import exceptions.NoSuchPlayerException;
+
 public class PriorityManager {
 	private static PriorityManager priorityManager;
+	private ArrayList<Player> playerList;
 
+	private PriorityManager() {
+		this.playerList = new ArrayList<Player>();
+	}
+	
 	public static PriorityManager getInstance() {
 		if (priorityManager == null) {
 			priorityManager = new PriorityManager();
 		}
 		return priorityManager;
+	}
+
+	public void removePlayer(Player player) throws NoSuchPlayerException {
+		if(this.playerList.contains(player)) {
+			this.playerList.remove(player);
+		} else {
+			throw new NoSuchPlayerException();
+		}
+	}
+
+	public static void tearDown() {
+		// TODO Auto-generated method stub
+		
 	}
 }

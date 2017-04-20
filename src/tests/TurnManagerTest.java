43683573dfcbeb38dce4afda06f0070184b91162
Aaron Michael <apmichael9@gmail.com>
2017-04-20 18:32:45 -0400

    Added testHandlesPlayers(). fails.

--- src/tests/TurnManagerTest.java
+++ src/tests/TurnManagerTest.java
@@ -3,8 +3,12 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
+import code.Player;
 import code.PlayerManager;
 import code.TurnManager;
 import code.TurnPriorityManager;
@@ -28,9 +32,15 @@ public class TurnManagerTest {
 	@Test
 	public void testHandlesPlayers() {
 
+		ArrayList<Player> players=new ArrayList<>();
+		players.add(new Player("Adam"));
+		players.add(new Player("Bob"));
+		players.add(new Player("Charlie"));
+		
 		PlayerManager pmgr = new PlayerManager();
+		pmgr.players=players;
 		TurnManager manager = new TurnManager();
 		manager.setPlayerManager(pmgr);
-
+		assertEquals("Adam",manager.getCurrentPlayer());
 	}
 }

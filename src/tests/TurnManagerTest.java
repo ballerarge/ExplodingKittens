9a90db0fb5f56450211d4403eef4e7c583b07379
Aaron Michael <apmichael9@gmail.com>
2017-04-21 12:49:48 -0400

    Added testEndTurnWithoutDraw() and testEndTurnAndDraw(). Both pass.

--- src/tests/TurnManagerTest.java
+++ src/tests/TurnManagerTest.java
@@ -32,7 +32,6 @@ public class TurnManagerTest {
 
 	@Test
 	public void testHandlesPlayers() {
-
 		ArrayList<Player> players = new ArrayList<>();
 		Player mockPlayer1 = EasyMock.mock(Player.class);
 		Player mockPlayer2 = EasyMock.mock(Player.class);
@@ -54,4 +53,62 @@ public class TurnManagerTest {
 		assertEquals(mockPlayer1, manager.getCurrentPlayer());
 	}
 
+	@Test
+	public void testEndTurnWithoutDraw() {
+		ArrayList<Player> players = new ArrayList<>();
+		Player mockPlayer1 = EasyMock.mock(Player.class);
+		Player mockPlayer2 = EasyMock.mock(Player.class);
+		Player mockPlayer3 = EasyMock.mock(Player.class);
+		players.add(mockPlayer1);
+		players.add(mockPlayer2);
+		players.add(mockPlayer3);
+		PlayerManager mockPM = EasyMock.mock(PlayerManager.class);
+		TurnManager manager = new TurnManager();
+
+		EasyMock.expect(mockPM.getPlayers()).andReturn(players);
+
+		EasyMock.replay(mockPM, mockPlayer1, mockPlayer2, mockPlayer3);
+
+		manager.setPlayerManager(mockPM);
+		assertEquals(mockPlayer1, manager.getCurrentPlayer());
+		manager.endTurnWithoutDraw();
+		assertEquals(mockPlayer2, manager.getCurrentPlayer());
+		manager.endTurnWithoutDraw();
+		assertEquals(mockPlayer3, manager.getCurrentPlayer());
+		manager.endTurnWithoutDraw();
+		assertEquals(mockPlayer1, manager.getCurrentPlayer());
+
+		EasyMock.verify(mockPM, mockPlayer1, mockPlayer2, mockPlayer3);
+	}
+	
+	@Test
+	public void testEndTurnAndDraw() {
+		ArrayList<Player> players = new ArrayList<>();
+		Player mockPlayer1 = EasyMock.mock(Player.class);
+		Player mockPlayer2 = EasyMock.mock(Player.class);
+		Player mockPlayer3 = EasyMock.mock(Player.class);
+		players.add(mockPlayer1);
+		players.add(mockPlayer2);
+		players.add(mockPlayer3);
+		PlayerManager mockPM = EasyMock.mock(PlayerManager.class);
+		TurnManager manager = new TurnManager();
+
+		EasyMock.expect(mockPM.getPlayers()).andReturn(players);
+		mockPlayer1.drawCard();
+		mockPlayer2.drawCard();
+		mockPlayer3.drawCard();
+		
+		EasyMock.replay(mockPM, mockPlayer1, mockPlayer2, mockPlayer3);
+
+		manager.setPlayerManager(mockPM);
+		assertEquals(mockPlayer1, manager.getCurrentPlayer());
+		manager.endTurnAndDraw();
+		assertEquals(mockPlayer2, manager.getCurrentPlayer());
+		manager.endTurnAndDraw();
+		assertEquals(mockPlayer3, manager.getCurrentPlayer());
+		manager.endTurnAndDraw();
+		assertEquals(mockPlayer1, manager.getCurrentPlayer());
+
+		EasyMock.verify(mockPM, mockPlayer1, mockPlayer2, mockPlayer3);
+	}
 }

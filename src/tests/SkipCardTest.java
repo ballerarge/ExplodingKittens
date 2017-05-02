2eace3c3aafab55a0d5039d2bd84e8abb4bf471a
decramrj <decramrj@rose-hulman.edu>
2017-05-02 14:35:03 -0400

    testSkipPlayedAfterAttack passes

--- src/tests/SkipCardTest.java
+++ src/tests/SkipCardTest.java
@@ -1,3 +1,4 @@
+
 package tests;
 
 import static org.junit.Assert.*;
@@ -16,41 +17,67 @@ import code.TurnManager;
 import exceptions.InvalidNumberofPlayersException;
 
 public class SkipCardTest {
-	
+	CardFactory factory;
+	Game game;
+	Player player1;
+	Player player2;
+	Player player3;
+	CardStack stack;
+	TurnManager turnManager;
+	PriorityManager pManager;
+
 	@Before
-	public void initialize() {
+	public void initialize() throws InvalidNumberofPlayersException {
 		TurnManager.tearDown();
 		PriorityManager.tearDown();
 		CardStack.tearDown();
+		factory = new CardFactory();
+		game = new Game();
+		stack = CardStack.getInstance();
+		turnManager = TurnManager.getInstance();
+		pManager = PriorityManager.getInstance();
+		game.start(3);
+		player1 = game.getCurrentTurnPlayer();
+		game.nextTurn();
+		player2 = game.getCurrentTurnPlayer();
+		game.nextTurn();
+		player3 = game.getCurrentTurnPlayer();
+		game.nextTurn();
 	}
-	
+
 	@After
 	public void tearDown() {
 		TurnManager.tearDown();
 		PriorityManager.tearDown();
 		CardStack.tearDown();
 	}
-	
 
 	@Test
-	public void testSkipEndsTurnWithNoDraw() throws InvalidNumberofPlayersException {
-		
-		Game game = new Game();
-		CardFactory factory = new CardFactory();
-		game.start(3);
+	public void testSkipEndsTurnWithNoDraw() {
 		Card skipCard = factory.createCard(CardFactory.SKIP_CARD);
-		CardStack stack = CardStack.getInstance();
-		PriorityManager pManager = PriorityManager.getInstance();
-		Player player1 = game.getCurrentTurnPlayer();
-		Player player2 = game.getPlayers().get(1);
 		int handSize = player1.getHand().size();
-		
+
 		stack.addCard(skipCard);
 		pManager.resolveCard();
-		
+
 		assertEquals(player1, game.getPlayers().get(0));
 		assertEquals(handSize, player1.getHand().size());
 		assertEquals(player2, game.getCurrentTurnPlayer());
+	}
+
+	@Test
+	public void testSkipPlayedAfterAttack() {
+		Card skipCard = factory.createCard(CardFactory.SKIP_CARD);
+		Card attackCard = factory.createCard(CardFactory.ATTACK_CARD);
+		
+		stack.addCard(attackCard);
+		pManager.resolveCard();
+		stack.addCard(skipCard);
+		pManager.resolveCard();
+		
+		assertEquals(player2, game.getCurrentTurnPlayer());
+		game.nextTurn();
+		assertEquals(player3, game.getCurrentTurnPlayer());
 	}
 
 }

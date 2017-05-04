52140bfc42d8f7434f8432f5a556a052c6b1c823
decramrj <decramrj@rose-hulman.edu>
2017-05-03 21:03:33 -0400

    testDefusePlayedWithKittenOnStack passes

--- src/code/DefuseCard.java
+++ src/code/DefuseCard.java
@@ -1,6 +1,8 @@
 
 package code;
 
+import java.util.Stack;
+
 public class DefuseCard extends Card {
 	public DefuseCard() {
 		this.cardID = 2;
@@ -8,7 +10,7 @@ public class DefuseCard extends Card {
 
 	@Override
 	public void cardAction(Player p1, Player p2) {
-
+		CardStack.getInstance().setStack(new Stack<Card>());
 	}
 
 	@Override

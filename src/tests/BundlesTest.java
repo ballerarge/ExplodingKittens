da50e1f610b03176f9b7937dc202d14e043fda5b
decramrj <decramrj@rose-hulman.edu>
2017-05-13 14:24:03 -0400

    Refactored previous test into three separate tests, all pass

--- src/tests/BundlesTest.java
+++ src/tests/BundlesTest.java
@@ -28,39 +28,56 @@ public class BundlesTest {
 		        new NormalCard(), new NormalCard()));
 
 		try {
-			TwoCardBundle twoBundle		= new TwoCardBundle(bundleTwo);
-			ThreeCardBundle threeBundle	= new ThreeCardBundle(bundleThree);
-			FiveCardBundle fiveBundle	= new FiveCardBundle(bundleFive);
+			TwoCardBundle twoBundle = new TwoCardBundle(bundleTwo);
+			ThreeCardBundle threeBundle = new ThreeCardBundle(bundleThree);
+			FiveCardBundle fiveBundle = new FiveCardBundle(bundleFive);
 		} catch (InvalidBundleException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Test(expected = InvalidBundleException.class)
-	public void testIncorrectBundleSize() throws InvalidBundleException {
+	public void testTwoBundleIncorrectBundleSize() throws InvalidBundleException {
 
 		ArrayList<Card> bundleWrong = new ArrayList<>(Arrays.asList(new NormalCard()));
 
-
-		TwoCardBundle twoBundle		= new TwoCardBundle(bundleWrong);
-		ThreeCardBundle threeBundle	= new ThreeCardBundle(bundleWrong);
-		FiveCardBundle fiveBundle	= new FiveCardBundle(bundleWrong);
+		TwoCardBundle twoBundle = new TwoCardBundle(bundleWrong);
 	}
-	
+
+	@Test(expected = InvalidBundleException.class)
+	public void testThreeBundleIncorrectBundleSize() throws InvalidBundleException {
+
+		ArrayList<Card> bundleWrong = new ArrayList<>(Arrays.asList(new NormalCard()));
+
+		ThreeCardBundle threeBundle = new ThreeCardBundle(bundleWrong);
+	}
+
+	@Test(expected = InvalidBundleException.class)
+	public void testFiveBundleIncorrectBundleSize() throws InvalidBundleException {
+
+		ArrayList<Card> bundleWrong = new ArrayList<>(Arrays.asList(new NormalCard()));
+
+		FiveCardBundle fiveBundle = new FiveCardBundle(bundleWrong);
+	}
+
 	@Test(expected = InvalidBundleException.class)
 	public void testIncorrectBundleCards() throws InvalidBundleException {
 
 		ArrayList<Card> bundleWrong = new ArrayList<>(Arrays.asList(new AttackCard(), new NormalCard()));
 
-
-		TwoCardBundle twoBundle		= new TwoCardBundle(bundleWrong);
-		ThreeCardBundle threeBundle	= new ThreeCardBundle(bundleWrong);
-		FiveCardBundle fiveBundle	= new FiveCardBundle(bundleWrong);
+		TwoCardBundle twoBundle = new TwoCardBundle(bundleWrong);
+		ThreeCardBundle threeBundle = new ThreeCardBundle(bundleWrong);
+		FiveCardBundle fiveBundle = new FiveCardBundle(bundleWrong);
 	}
-	
+
 	@Test(expected = InvalidBundleException.class)
 	public void testTwoCardNullBundleCards() throws InvalidBundleException {
 		TwoCardBundle twoBundle = new TwoCardBundle(null);
+	}
+
+	@Test(expected = InvalidBundleException.class)
+	public void testThreeCardNullBundleCards() throws InvalidBundleException {
+		ThreeCardBundle threeBundle = new ThreeCardBundle(null);
 	}
 
 }

f17f1a8ed0fb26b6256d6285393e567c85829c05
decramrj <decramrj@rose-hulman.edu>
2017-05-14 21:32:05 -0400

    testMoveSelectedToStackTwoNormal passes

--- src/tests/HandTest.java
+++ src/tests/HandTest.java
@@ -1,16 +1,49 @@
 
 package tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import code.AttackCard;
+import code.Card;
+import code.CardFactory;
+import code.CardStack;
+import code.DiscardDeck;
 import code.Hand;
+import code.MainDeck;
+import code.PriorityManager;
+import code.TurnManager;
 import exceptions.IncorrectNumberOfCardsException;
 import exceptions.InvalidBundleException;
 import exceptions.NoCardsToMoveException;
 
 public class HandTest {
+	
+	@Before
+	public void initialize() {
+		TurnManager.tearDown();
+		MainDeck.tearDown();
+		DiscardDeck.tearDown();
+		PriorityManager.tearDown();
+		CardStack.tearDown();
+	}
+	
+	@After
+	public void tearDown() {
+		TurnManager.tearDown();
+		MainDeck.tearDown();
+		DiscardDeck.tearDown();
+		PriorityManager.tearDown();
+		CardStack.tearDown();
+	}
 
 	@Test
 	public void testHandManagerCreation() {
@@ -52,10 +85,14 @@ public class HandTest {
 	}
 
 	@Test
-	public void testMoveSelectedToStack() throws IncorrectNumberOfCardsException, NoCardsToMoveException, InvalidBundleException {
+	public void testMoveSelectedToStackTwoNormal() throws IncorrectNumberOfCardsException, NoCardsToMoveException, InvalidBundleException {
 		Hand handMng = new Hand();
-
-		handMng.draw();
+		CardFactory factory = new CardFactory();
+		List<Card> cards = new ArrayList<Card>();
+		cards.add(factory.createCard(CardFactory.NORMAL_CARD));
+		cards.add(factory.createCard(CardFactory.NORMAL_CARD));
+		handMng.addCards(cards);
+		handMng.selectCard(1);
 		handMng.selectCard(0);
 
 		handMng.moveSelectedToStack();
@@ -68,6 +105,20 @@ public class HandTest {
 		Hand handMng = new Hand();
 
 		handMng.moveSelectedToStack();
+	}
+	
+	@Test
+	public void testMoveSelectedToStackNonNormal() throws NoCardsToMoveException, InvalidBundleException {
+		Hand handMng = new Hand();
+		CardFactory factory = new CardFactory();
+		List<Card> cards = new ArrayList<Card>();
+		cards.add(factory.createCard(CardFactory.ATTACK_CARD));
+		handMng.addCards(cards);
+		
+		handMng.selectCard(0);
+		handMng.moveSelectedToStack();
+		
+		assertEquals(0, handMng.getSelectedCards().size());
 	}
 
 }

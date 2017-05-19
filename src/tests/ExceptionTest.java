c539e35e18249ef4b7d9ea5e51e6ebe9573b62d5
decramrj <decramrj@rose-hulman.edu>
2017-05-18 23:59:12 -0400

    testIncorrectCardIconIDExceptionMessage passes

--- src/tests/ExceptionTest.java
+++ src/tests/ExceptionTest.java
@@ -10,5 +10,10 @@ public class ExceptionTest {
 	public void testIncorrectCardIconIDException() throws IncorrectCardIconIDException {
 		throw new IncorrectCardIconIDException();
 	}
+	
+	@Test(expected = IncorrectCardIconIDException.class)
+	public void testIncorrectCardIconIDExceptionMessage() throws IncorrectCardIconIDException {
+		throw new IncorrectCardIconIDException("Error");
+	}
 
 }

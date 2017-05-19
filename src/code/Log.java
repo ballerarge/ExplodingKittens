867c6ce851d74c632f5e84cf57d5a1c895f85555
jwile14 <wilejd@wilejd-w540.rose-hulman.edu>
2017-05-18 20:29:52 -0400

    Updated Log to print out in reverse

--- src/code/Log.java
+++ src/code/Log.java
@@ -7,34 +7,33 @@ import java.util.Locale;
 public class Log {
 
 	private static Log log;
-	private ArrayList<Entry> entries;//0 is the earliest event
+	private ArrayList<Entry> entries;// 0 is the earliest event
 	public Locale locale = Locale.ENGLISH;
-	
-	
+
 	public static Log getInstance() {
 		if (log == null)
 			log = new Log();
 		return log;
 	}
-	
-	public static void tearDown(){
+
+	public static void tearDown() {
 		log = null;
 	}
-	
-	public static void setLog(Log otherLog) {//for testing purposes
+
+	public static void setLog(Log otherLog) {// for testing purposes
 		log = otherLog;
 	}
-	
+
 	private Log() {
 		entries = new ArrayList<Entry>();
 	}
-	
-	public void addEntry(Entry entry){
-		entries.add(entry);
+
+	public void addEntry(Entry entry) {
+		entries.add(0, entry);
 	}
-	
-	public ArrayList<Entry> getEntries(){
+
+	public ArrayList<Entry> getEntries() {
 		return entries;
 	}
-	
+
 }
